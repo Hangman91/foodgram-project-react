1386e4665f64ece94d54deaedad510423e1d0203
Alexander Dementev <astalavista_91@mail.ru>
2022-08-16 00:03:36 +0300

    add recipes models

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, Ingredient, Tag
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'text', 'author', 'tags') 
+    search_fields = ('name',) 
+    list_filter = ('tags',) 
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit') 
+    search_fields = ('name',) 
+    list_filter = ('name',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug') 
+    search_fields = ('name',) 
+    list_filter = ('name',)
+
+
+admin.site.register(Recipe, RecipeAdmin) 
+admin.site.register(Ingredient, IngredientAdmin) 
+admin.site.register(Tag, TagAdmin) 
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator 
+User = get_user_model()
 
-# Create your models here.
+
+class Recipe(models.Model):
+
+    """Класс Recipe, то есть Рецепт.
+    Содержит следующие поля:
+    *Автор публикации (пользователь).
+    *Название.
+    *Картинка.
+    *Текстовое описание.
+    *Ингредиенты: продукты для приготовления блюда по рецепту. 
+    Множественное поле, выбор из предустановленного списка,
+    с указанием количества и единицы измерения.
+    *Тег (можно установить несколько тегов на один рецепт,
+    выбор из предустановленных).
+    *Время приготовления в минутах"""
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+
+    name = models.CharField(
+        max_length=200
+    )
+    #image = pass
+    text = models.TextField()
+    ingredients = models.ManyToManyField(
+        'Ingredient',
+        #on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='recipes'
+    )
+    tags = models.ForeignKey(
+        'Tag',
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='recipes'
+    )
+    cooking_time = models.IntegerField(
+        validators=[MinValueValidator(1)]
+    )
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+
+    name = models.CharField(
+        max_length=200,
+        unique=True
+    )
+    slug = models.SlugField(
+        unique=True
+    )
+    color = models.CharField(
+        max_length=7,
+        unique=True
+    )
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+
+    name = models.CharField(
+        max_length=200,
+        unique=True
+    )
+
+    measurement_unit = models.CharField(
+        max_length=200
+    )
+
+    amount = models.IntegerField(
+        validators=[MinValueValidator(1)]
+    )
+    def __str__(self):
+        return self.name
