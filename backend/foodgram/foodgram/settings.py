5b2a178e04b57bced37f45e3134e63c2f5135d1f
Alexander Dementev <astalavista_91@mail.ru>
2022-09-06 00:11:30 +0300

    fuckstatic

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -145,6 +145,9 @@ USE_TZ = True
 STATIC_URL = '/backend_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
+MEDIA_URL = '/backend_media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
+
 AUTH_USER_MODEL = 'users.User'
 
 DJOSER = {
