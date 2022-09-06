e605b28ac2a250cbea786919c1a66c310f2dc668
Alexander Dementev <astalavista_91@mail.ru>
2022-09-07 00:55:26 +0300

    add ings

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/', include('users.urls')),
-    path(
-        'api/redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc',
-    )
 ]
