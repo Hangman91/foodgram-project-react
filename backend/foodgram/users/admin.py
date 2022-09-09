3d184c2cfc2f06a85fb7373e3b58e0f830077a85
Alexander Dementev <astalavista_91@mail.ru>
2022-09-09 13:38:40 +0300

    Админка

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-#from rest_framework.authtoken.models import Token
 from users.models import Follow, User
 
 
@@ -26,4 +25,3 @@ class FollowAdmin(admin.ModelAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Follow, FollowAdmin)
-#admin.site.unregister(Token)
